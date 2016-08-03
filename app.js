02227f7e88289aa925932452c896b114e156a302
Danielle Bowman <dcbowman6@yahoo.com>
2016-08-02 22:11:33 -0400

    routes set up

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
-var cheerio = require('cheerio');
-var request = require('request');
+// var cheerio = require('cheerio');
+// var request = require('request');
 
-//Initialize express
-var express = require('express');
-var app = express();
-
-
-//pulls info using request npm from website
-request('http://elitedaily.com/category/news/', function(err, res, body){
-	var $ = cheerio.load(body);
-	var results = []
-
-	//pulls all of the h2 values for the body of the site
-	$('h2').each(function(i, element){
-		//console.log($(this).text());
+// //Initialize express
+// var express = require('express');
+// var app = express();
 
 
-		//grabs the text of all of the h2 values and looks for the link that is related to those values
-		var title = $(this).text();
-		var link = $(this).find('a').attr('href');
+// //pulls info using request npm from website
+// request('http://elitedaily.com/category/news/', function(err, res, body){
+// 	var $ = cheerio.load(body);
+// 	var results = []
 
-		//pushes those values into the results array
-		results.push({
-			title: title,
-			url: link
-		});
+// 	//pulls all of the h2 values for the body of the site
+// 	$('h2').each(function(i, element){
+// 		//console.log($(this).text());
 
-		//check correct info is being scraped
-		console.log(results);
-	});
-});
 
-//saves the URL of our database and names the collection
+// 		//grabs the text of all of the h2 values and looks for the link that is related to those values
+// 		var title = $(this).text();
+// 		var link = $(this).find('a').attr('href');
 
-var databaseURL = "news";
-var collections = "stories";
+// 		//pushes those values into the results array
+// 		results.push({
+// 			title: title,
+// 			url: link
+// 		});
 
-//uses mongojs to hook the db to the db variable
-var db = mongojs(databaseURL, collections);
+// 		//check correct info is being scraped
+// 		console.log(results);
+// 	});
+// });
 
-//ensures any errors are logged if mongodb runs into an issue
-db.on('error', function(err){
-	console.log("Database Error:", err);
-});
+// //saves the URL of our database and names the collection
+
+// var databaseURL = "news";
+// var collections = "stories";
+
+// //uses mongojs to hook the db to the db variable
+// var db = mongojs(databaseURL, collections);
+
+// //ensures any errors are logged if mongodb runs into an issue
+// db.on('error', function(err){
+// 	console.log("Database Error:", err);
+// });
--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 //Dependencies
+//Initalize Express
 var express = require('express');
 var app = express();
+
+//Data for server
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 var path = require('path');
+
+//Scraper for website
 var cheerio = require('cheerio');
 var request = require('request');
+
+//Database config
 var mongojs = require('mongojs');
+var databaseURL = "news";
+var collections = ["stories"];
+
 //Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + '/public'));
 
@@ -17,14 +27,47 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
-
+//Connects mongojs config to the db variable
+var db = mongojs(databaseURL, collections);
+db.on('error', function(err){
+	console.log("Database Error:", err);
+});
 
 // Routes
 
-app.get('/', function(req, res) {
-  res.render(index);
+//Route 1 retrieves all of the data from the scrapedData collection as a json
+app.get('/all', function(req, res) {
+  db.stories.find({})
+
+  res.send()
 });
 
+//Route 2 scrapes data from website and saves it to MongoDB
+app.get('/scrape', function(req, res){
+
+	//pulls info using request npm from website
+request('http://elitedaily.com/category/news/', function(err, res, body){
+	var $ = cheerio.load(body);
+	
+	//pulls all of the h2 values for the body of the site
+	$('h2').each(function(i, element){
+		//console.log($(this).text());
+
+
+		//grabs the text of all of the h2 values and looks for the link that is related to those values
+		var title = $(this).text();
+		var link = $(this).find('a').attr('href');
+
+		//pushes those values into the results array
+		collection.insert(title, function(err, result){
+			console.log(error);
+			console.log(result);
+		});
+		
+	  });
+
+	});
+});
 
 
 
